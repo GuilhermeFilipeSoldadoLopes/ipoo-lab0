6ce63f0bb32288d657a7006f5afe15ad8b3fb61d
GuilhermeFilipeSoldadoLopes <200221015@estudantes.ips.pt>
2021-03-04 09:55:55 +0000

    Initial sharing of project

---
+++ Enum.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Enum - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Enum
+{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
